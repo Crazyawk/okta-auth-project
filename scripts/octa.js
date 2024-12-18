fe2cda30bf2086b3691ff2173fdd5df5d4f3dc9d
Crazyawk <143974450+Crazyawk@users.noreply.github.com>
2024-12-18 17:30:22 -0500

    Added authentication

---
+++ scripts/octa.js
@@ -0,0 +1,11 @@
+// Initialize OktaAuth
+const oktaAuth = new OktaAuth({
+    issuer: 'dev-ap8jdoc17p5j43jc.us.auth0.com', // Replace with your Okta Issuer URL
+    clientId: 'T1QrAazxtZlFhmsFGNzawdVvs2gsIr7W', // Replace with your Okta Client ID
+    redirectUri: 'https://your-callback-url', // Replace with your callback URL
+});
+
+// Log in when the button is clicked
+document.getElementById('login-btn').addEventListener('click', () => {
+    oktaAuth.signInWithRedirect();
+});
